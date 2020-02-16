d2e60028e05b622accc696522899c391bf84c3de
Lam <626076005@qq.com>
2020-02-16 15:04:57 +0800

    第一次修改

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
 //app.js
 App({
+
+  //小程序初始化完成时，会执行的生命周期函数
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    //异步操作
+    // wx.getUserInfo({
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+    setTimeout(function(){
+      const err = new Error
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+      throw err
+    },2000)
+  },
+  //小程序界面显示出来之后会执行的生命周期函数
+  onShow:function(){
+    console.log('界面显示出来：onShow')
+  },
+  //界面被隐藏时被执行
+  onHide:function(){
+    console.log('界面别隐藏时执行：onHide')
+  },
+  //发生错误的时候被执行
+  onError:function(msg){
+    console.log('小程序发生了一些小错误')
   },
   globalData: {
     userInfo: null
