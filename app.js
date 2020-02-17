26384c7119d5e9586236b1e2107a24d75345cc2d
Lam <626076005@qq.com>
2020-02-17 14:16:35 +0800

    the #1

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ App({
     //     console.log(res)
     //   }
     // })
-    setTimeout(function(){
-      const err = new Error
+    // setTimeout(function(){
+    //   const err = new Error
 
-      throw err
-    },2000)
+    //   throw err
+    // },2000)
   },
   //小程序界面显示出来之后会执行的生命周期函数
-  onShow:function(){
-    console.log('界面显示出来：onShow')
+  onShow:function(options){
+    //判断小程序的进入场景
+    console.log(options)
+    switch(options){
+      case 1001 :
+      break
+    }
   },
   //界面被隐藏时被执行
   onHide:function(){
@@ -28,6 +33,8 @@ App({
     console.log('小程序发生了一些小错误')
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    name:'wingcheung',
+    age:24
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -32,5 +32,56 @@ Page({
     this.setData({
       counter: this.data.counter -= 1
     })
+  },
+  handgetuserinfo(event){
+    console.log(event)
+
+  },
+  clickhahaha(){
+    console.log('clickhahaha被点击了')
+  },
+  //页面加载出来时
+  onLoad(){
+    console.log('onLoad')
+    wx.request({
+      url:'http://v.juhe.cn/toutiao/index',
+      success:function(res){
+        console.log(res)
+      }
+    })
+  },
+  //页面显示出来时
+  onShow(){
+    console.log('onShow')
+  },
+  //页面初次渲染完成时
+  onReady(){
+    console.log('onReady')
+  },
+  //当页面隐藏起来时
+  onHide(){
+    console.log('onHide')
+  },
+  //当页面销毁时
+  onUnload(){
+    console.log('onUnload')
+  },
+
+
+
+  //监听页面的滚动
+  onPageScroll(obj){
+    // console.log(obj)
+  },
+
+
+
+  //监听页面滚动到底部
+  onReachBottom(){
+    console.log('页面滚动到底部')
+  },
+  onPullDownRefresh(){
+    console.log('下拉刷新事件')
   }
+
 })
